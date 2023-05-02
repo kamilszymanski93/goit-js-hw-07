e391f557f9b843bb2a43970eae5a2744e35ef4c0
Kamil Szymański <kamil313313@gmail.com>
2023-05-02 14:10:03 +0200

    HW7 almost done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,41 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryClass = document.querySelector(".gallery");
 
-console.log(galleryItems);
+let markup = "";
+galleryItems.forEach(({ preview, original, description }) => {
+  const newImage = `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`;
+  markup += newImage;
+});
+console.log("markup: ", markup);
+galleryClass.insertAdjacentHTML("beforeend", markup);
+const imageClick = document.querySelector(".gallery");
+
+imageClick.addEventListener("click", selectImage);
+
+function selectImage(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  const clickedImage = event.target.getAttribute("data-source");
+  const instance = basicLightbox.create(`
+    <img src="${clickedImage}" width="800" height="600">
+`);
+  instance.show();
+
+  galleryClass.addEventListener("keydown", (event) => {
+    if (event.key === "Escape") {
+      instance.close();
+    }
+  });
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,22 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryClass = document.querySelector(".gallery");
 
-console.log(galleryItems);
+let markup = "";
+galleryItems.forEach(({ preview, original, description }) => {
+  const newImage = `<li class="gallery__item">
+<a href="${original}"><img src="${preview}" alt="${description}" title="${description}"/></a>
+</li>`;
+  markup += newImage;
+});
+console.log("markup: ", markup);
+galleryClass.insertAdjacentHTML("beforeend", markup);
+const imageClick = document.querySelector(".gallery");
+
+new SimpleLightbox(".gallery a", {
+  captionDelay: 250,
+});
+
+// <a class="gallery__item" href="${original}">
+//   <img class="gallery__image" src="${preview}" alt="${description}" />
+//  </a>
