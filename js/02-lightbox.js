5fc5307306a8ad65106a9fdc4a5026cc54366275
Kamil Szymański <kamil313313@gmail.com>
2023-05-02 14:34:35 +0200

    HW7T2 Done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -5,18 +5,13 @@ const galleryClass = document.querySelector(".gallery");
 let markup = "";
 galleryItems.forEach(({ preview, original, description }) => {
   const newImage = `<li class="gallery__item">
-<a href="${original}"><img src="${preview}" alt="${description}" title="${description}"/></a>
-</li>`;
+  <a href="${original}"><img class="gallery__image" src="${preview}" alt="${description}" title="${description}"/></a>
+  </li>`;
   markup += newImage;
 });
 console.log("markup: ", markup);
 galleryClass.insertAdjacentHTML("beforeend", markup);
-const imageClick = document.querySelector(".gallery");
 
 new SimpleLightbox(".gallery a", {
   captionDelay: 250,
 });
-
-// <a class="gallery__item" href="${original}">
-//   <img class="gallery__image" src="${preview}" alt="${description}" />
-//  </a>
